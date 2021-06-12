b618e4009db11e72bc6d2fffce10e19b97f8b866
Giulia <giuliatullia93@gmail.com>
2021-06-12 18:35:23 +0200

    add-doppi-random

--- js/script.js
+++ js/script.js
@@ -5,13 +5,28 @@
 
 var casuali = [];
 
-for (var i = 0; i < 5; i++){
-    casuali.push(Math.floor(Math.random() * 100) + 1);
-}
-
-alert(casuali);
+   for (var i = 0; i < 5; i++) {
+    var n =  Math.floor(Math.random() *100) + 1;
+    var check = casuali.includes(n);
+  
+  if(check === false) {
+    casuali.push(n);
+  } else {
+    while(check === true){
+      n = Math.floor(Math.random() * 100) + 1;
+      check = casuali.includes(n);
+        if(check === false){
+          casuali.push(n);
+        }
+      }
+    }
+  }
+     
 
 console.log(casuali);
+alert(casuali);
+
+
 
 setTimeout(ritarda, 1000);
 
@@ -19,18 +34,22 @@ function ritarda(){
 
    var memoria = [];
 
-   for (var i = 0; i < 5; i++){
-       var ricordati = parseInt(prompt('scrivi il numero'));
+   for (var i = 1; i <= 5; i++){
+       var ricordati = parseInt(prompt('scrivi il numero' + ' ' + i));
        if(casuali.includes(ricordati)){
         memoria.push(ricordati);
     }
  }
- alert('ne hai indovinati'+ ' ' + memoria.length + ' ' + 'I numeri sono' + ' ' + memoria);
- console.log(memoria);
+
+ 
+ alert('ne hai indovinati  '+ ' ' + memoria.length + ' ' + 'I numeri sono: ' + ' ' + memoria + '  -  ' + 'i numeri del computer sono: ' + casuali);
+ 
 }
         
-
+//BONUS controllare che i numeri generati casualmente siano univoci (non ci siano doppioni) ed anche i numeri che l'utente inserisce non devono già essere stati scritti precedentemente.
        
+
+
 
 
 
