5521432127c43beb4184e4713e6d2d53a833f47f
Giulia <giuliatullia93@gmail.com>
2021-06-12 17:43:33 +0200

    add-fine-esercizio

--- js/script.js
+++ js/script.js
@@ -3,44 +3,58 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-
-
-// faccio un array vuoto, con un ciclo for creo i 5 numeri casuali
-
-
 var casuali = [];
 
 for (var i = 0; i < 5; i++){
     casuali.push(Math.floor(Math.random() * 100) + 1);
-
 }
 
 alert(casuali);
 
 console.log(casuali);
 
-
-// funzione che ritarda l'inizio del prompt di 30 secondi
-
 setTimeout(ritarda, 1000);
-var memoria = [];
-
 
 function ritarda(){
-    var memoria = [];
+
+   var memoria = [];
+
    for (var i = 0; i < 5; i++){
        var ricordati = parseInt(prompt('scrivi il numero'));
-       memoria.push(ricordati);
-   }
-
-console.log(memoria);
-if(casuali === memoria ){
-    alert('hai vinto');
-}else{
-    alert('hai perso');
+       if(casuali.includes(ricordati)){
+        memoria.push(ricordati);
+    }
+ }
+ alert('ne hai indovinati'+ ' ' + memoria.length + ' ' + 'I numeri sono' + ' ' + memoria);
+ console.log(memoria);
 }
+        
 
-}
+       
+
+
+
+
+
+
+
+
+
+
+       
+
+   
+    
+
+
+
+
+
+
+
+      
+
+
 
 
 
